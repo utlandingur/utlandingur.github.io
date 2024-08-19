1d241764a391dda8694102dfd96ff60eb403784a
Luke Hening <152434106+utlandingur@users.noreply.github.com>
2024-08-19 12:39:42 +0000

    Rewrite some text and remove overlay on small screens

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
-// Remove overlay filter on images when hovering with a mouse
-// Add them again when no longer hovering
-
+// get all elements with class "overlay"
 const overlaysArray = Array.from(document.getElementsByClassName("overlay"));
 
-const removeOverlay = (event) => {
+if (window.innerWidth >= 800) {
+  // Remove overlay filter on images when hovering with a mouse
+  // Add them again when no longer hovering
+
+  const removeOverlay = (event) => {
     const overlay = event.currentTarget;
     overlay.style.opacity = "0";
-};
+  };
 
-const addOverlay = (event) => {
+  const addOverlay = (event) => {
     const overlay = event.currentTarget;
     overlay.style.opacity = "";
-};
+  };
 
-const handleOverlayEvents = (overlay) => {
-    overlay.addEventListener("mouseenter",removeOverlay);
+  const handleOverlayEvents = (overlay) => {
+    overlay.addEventListener("mouseenter", removeOverlay);
     overlay.addEventListener("mouseleave", addOverlay);
-}
+  };
 
-overlaysArray.forEach(overlay => handleOverlayEvents(overlay));
+  overlaysArray.forEach((overlay) => handleOverlayEvents(overlay));
+} else {
+  // Remove overlay filter on images when on mobile or small screens
+  overlaysArray.forEach((overlay) => (overlay.style.opacity = "0"));
+}
